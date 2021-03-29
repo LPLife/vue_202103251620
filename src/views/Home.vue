709d0c2b6ccad05d0e67813b128ba19c3fbc5ea5
lupei <peri.lu@mart.blue>
2021-03-29 18:30:19 +0800

    ...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <div class="demo_1">
+            <p>dddddd</p>
+            <p>ddddddd</p>
+            <p>dddd</p>
+            <p>ddddd</p>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+    name: 'Home',
+
+    components: {
+        HelloWorld
+    }
 };
 </script>
+<style lang="less" scoped>
+p {
+    &:nth-of-type(even) {
+        border-bottom: 1px solid #dddddd;
+    }
+}
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const {name} = require('./package');
 module.exports = {
-  lintOnSave: false,
+    lintOnSave: true,
+    devServer: {
+        port: 8037,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    },
+    configureWebpack: {
+        output: {
+            library: `${name}-[name]`,
+            libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+            jsonpFunction: `webpackJsonp_${name}`
+        }
+    }
 };
